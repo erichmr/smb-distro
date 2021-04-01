f40eb1b428431f2f6ec7c5981ecb6feeec631e4b
erichmr <77982219+erichmr@users.noreply.github.com>
2021-04-01 11:51:45 -0300

    update file

--- mkdistro_smb.py
+++ mkdistro_smb.py
@@ -1,22 +1,29 @@
 # libraries
 import csv
 import json
+from datetime import datetime
 import matplotlib as plt
 import pandas as pd
 import pytz
 import requests
-import rows
+# import rows
 import time
 
-from datetime import datetime
-
 # variables
-olist_all, olister_vacation, olister_off, error_box = []
+olist_all = []
+olister_vacation = []
+olister_off = []
+error_box = []
+
 count_succ = 0
 count_fail = 0
 
 ttm = datetime.now()
-print("Local:", ttm.strftime("%m/%d/%Y, %H:%M:%S"))
+## print("Local:", ttm.strftime("%m/%d/%Y, %H:%M:%s"))
+
+localtime = ttm.strftime("%m/%d/%Y %H:%M:%S")
+print(localtime)
+
 
 # read flat file (csv)
 ## read csv with data tp change
