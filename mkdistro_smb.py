d49ea82d5f339369038f46448e8d746b78adb6e4
erichmr <77982219+erichmr@users.noreply.github.com>
2021-04-01 11:12:14 -0300

    create file for mkdistro_smb.py
    initial file with a initial approach to automatize this task, which demands 1 hour per day, only on this process.

---
+++ mkdistro_smb.py
@@ -0,0 +1,35 @@
+# libraries
+import csv
+import datetime
+import json
+import matplotlib as plt
+import pandas as pd
+import requests
+import rows
+import time
+
+# variables
+olist_all, olister_vacation, olister_off, error_box = []
+count_succ = 0
+count_fail = 0
+
+# read flat file (csv)
+## read csv with data tp change
+## validate errors (local)
+## remove blanks fields
+
+
+# pre processing conditions
+## read arrays, show
+## error_box handle fail executions
+
+
+# post processing conditions
+## calculations
+# conditions
+
+# final results
+## get output data
+## get output chart
+## get output log (local file only)
+
